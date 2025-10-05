499210edaf9b106ecaa83e5753e7a6cc90883c1e
Rukmana <maakunn470@gmail.com>
2025-10-05 21:17:31 +0700

    Update supabaseClient.ts

--- src/app/lib/supabaseClient.ts
+++ src/app/lib/supabaseClient.ts
@@ -1,10 +1,12 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
- 
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY
+
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Supabase URL atau ANON KEY belum di-set di .env.local');
+  throw new Error('Supabase URL atau ANON KEY belum di-set di environment!')
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
